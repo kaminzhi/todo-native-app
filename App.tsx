c9701a86f4c0b2e05343238017b72fb804f1d6b5
kaminzhi <issac20050710@gmail.com>
2024-10-27 05:33:43 +0800

    feat: navbar, sidebar, menuicon

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import AppContainer from "./src/components/app-containers";
 import React from "react";
-import Main from "./src/screens/mainscreen";
+import AppContainer from "./src/components/app-containers";
+import Navigator from "./src/";
 
 export default function App() {
   return (
     <AppContainer>
-      <Main />
+      <Navigator />
     </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
